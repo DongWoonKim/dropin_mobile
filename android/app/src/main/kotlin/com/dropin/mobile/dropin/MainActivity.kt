97cea8b2cf6afe7f4aa5565900258100c348ad8f
DongWoonKim <01030985843z@naver.com>
2023-12-10 17:17:33 +0900

    prj setup

---
+++ android/app/src/main/kotlin/com/dropin/mobile/dropin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dropin.mobile.dropin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/start.dart
@@ -0,0 +1,10 @@
+printInteger(int aNumber) {
+  print('The Number is $aNumber.');
+}
+
+
+
+main() {
+  var number = 42;
+  // printInteger(number);
+}
